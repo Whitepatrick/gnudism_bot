a4920fae88161af4083bdc8024da42f4b34b508e
Patrick White <whitep@vcu.edu>
2015-09-09 07:48:01 -0400

    created main class, includes a huge connection block, needs refactoring

--- gnudism_bot.rb
+++ gnudism_bot.rb
@@ -2,20 +2,25 @@ require 'cinch'
 require 'cinch/plugins/identify'
 require_relative 'credentials'
 
-
-bot = Cinch::Bot.new do
-  configure do |c|
-    # add all required options here
-    c.plugins.plugins = [Cinch::Plugins::Identify] # optionally add more plugins
-    c.plugins.options[Cinch::Plugins::Identify] = {
-      :username => USERNAME,
-      :password => PASSWORD,
-      :type     => :nickserv,
-    }
-    c.nick = USERNAME
-    c.server = "irc.freenode.org"
-    c.channels = ["#gnudism"]
+class GnudismBot
+  def initialize
+    bot = Cinch::Bot.new do
+      configure do |c|
+        # add all required options here
+        c.plugins.plugins = [Cinch::Plugins::Identify] # optionally add more plugins
+        c.plugins.options[Cinch::Plugins::Identify] = {
+          :username => USERNAME,
+          :password => PASSWORD,
+          :type     => :nickserv,
+        }
+        c.nick = USERNAME
+        c.server = "irc.freenode.org"
+        c.channels = ["#gnudism"]
+      end
+    end
+    bot.start
   end
 end
 
-bot.start
+gb = GnudismBot.new
+gb
